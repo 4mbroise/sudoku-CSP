87175d79ba27f22f814d1529b347e9330199787b
Elio <elliotfaugier@gmail.com>
2023-09-28 17:15:24 +0200

    ok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,46 +2,84 @@ package org.example;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.IntVar;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) {
+    static int L = 9;
+    public static void main(String[] args) throws CsvValidationException, IOException {
 
-        readSudoKuCsv("sudoku_cluewise.csv");
+        SudokuRepository repo = readSudoKuCsv("sudoku_cluewise.csv");
 
-        System.out.println("Hello world!");
+        solve(repo.getRandomSudoku(17));
+
+        System.out.println("END");
     }
 
 
-    public static SudokuRepositoory readSudoKuCsv(String file) {
-        try {
+    public static  void solve(String sudokuAsString) {
+        Model model = new Model("Sudoku");
 
-            SudokuRepositoory repo = new SudokuRepositoory();
-
-            FileReader fileReader = new FileReader(file);
-            CSVReader csvReader = new CSVReaderBuilder(fileReader).withSkipLines(1).build();
-
-            String[] nextRecord;
-
-            while ((nextRecord = csvReader.readNext()) != null) {
-                for (String cell : nextRecord) {
-                    System.out.print(cell + "\t");
+        IntVar[][] sudokuBoard = new IntVar[L][L];
+        for (int i = 0; i<L; i++) {
+            if (i==3 || i==6){
+                System.out.println("------+-------+------");
+            }
+            for(int j = 0; j<L; j++) {
+                int cellValue = Character.getNumericValue(sudokuAsString.charAt(i*9+j));
+                // Unknown value => we create a variable
+                if(cellValue == 0){
+                    sudokuBoard[i][j] = model.intVar("X_"+i+"_"+j,1,9);
+                }
+                // Knwon value => we create a constant
+                else {
+                    sudokuBoard[i][j] = model.intVar("X_"+i+"_"+j, cellValue);
                 }
 
-                String sudoku = nextRecord[0];
-                String nbClue = nextRecord[2] ;
+                if(j==3 || j == 6) {
+                    System.out.print("| ");
+                }
+                if(cellValue != 0){
+                    System.out.print(cellValue);
+                } else{
+                    System.out.print(" ");
+                }
 
-                repo.add(Integer.parseInt(nbClue), sudoku);
-
-                System.out.println();
-                break;
+                System.out.print(" ");
             }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println();
         }
+
+        System.out.println(sudokuBoard[0][0]);
+
+    }
+
+    public static SudokuRepository readSudoKuCsv(String file) throws IOException, CsvValidationException {
+
+        SudokuRepository repo = new SudokuRepository();
+
+        FileReader fileReader = new FileReader(file);
+        CSVReader csvReader = new CSVReaderBuilder(fileReader).withSkipLines(1).build();
+
+        String[] nextRecord;
+
+        while ((nextRecord = csvReader.readNext()) != null) {
+            String sudoku = nextRecord[0];
+            String nbClue = nextRecord[2] ;
+
+            repo.add(Integer.parseInt(nbClue), sudoku);
+
+            //System.out.println();
+        }
+
+        return repo;
+
     }
 
 }
